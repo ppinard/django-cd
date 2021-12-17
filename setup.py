c7e4a3f8c59b665d82d4b387cdfd57fc803547f1
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-17 14:25:47 +0000

    Fix installation

--- setup.py
+++ setup.py
@@ -12,51 +12,57 @@ import versioneer
 # Globals and constants variables.
 BASEDIR = Path(__file__).parent.resolve()
 
-with open(BASEDIR.joinpath('README.md'), 'r') as fp:
+with open(BASEDIR.joinpath("README.md"), "r") as fp:
     LONG_DESCRIPTION = fp.read()
 
-PACKAGES = find_packages()
+PACKAGES = find_packages(exclude=["project", "tests", "tests.*"])
 
-with open(BASEDIR.joinpath('requirements.txt'), 'r') as fp:
+PACKAGE_DATA = {
+    "django_cd": [
+        "templates/*.html",
+        "templates/django_cd/*.html",
+        "static/*.svg",
+        "static/*.ico",
+        "static/*.css",
+    ],
+}
+
+with open(BASEDIR.joinpath("requirements.txt"), "r") as fp:
     INSTALL_REQUIRES = fp.read().splitlines()
 
 EXTRAS_REQUIRE = {}
-with open(BASEDIR.joinpath('requirements-dev.txt'), 'r') as fp:
-    EXTRAS_REQUIRE['dev'] = fp.read().splitlines()
-with open(BASEDIR.joinpath('requirements-test.txt'), 'r') as fp:
-    EXTRAS_REQUIRE['test'] = fp.read().splitlines()
+with open(BASEDIR.joinpath("requirements-dev.txt"), "r") as fp:
+    EXTRAS_REQUIRE["dev"] = fp.read().splitlines()
+with open(BASEDIR.joinpath("requirements-test.txt"), "r") as fp:
+    EXTRAS_REQUIRE["test"] = fp.read().splitlines()
 
 CMDCLASS = versioneer.get_cmdclass()
 
 ENTRY_POINTS = {}
 
 setup(
-    name='django-cd',
+    name="django-cd",
     version=versioneer.get_version(),
-    url='https://github.com/ppinard/django-cd',
+    url="https://github.com/ppinard/django-cd",
     author="Philippe Pinard",
-    author_email='philippe.pinard@gmail.com',
+    author_email="philippe.pinard@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
     description="Continuous deloyment",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     license="MIT license",
-
     packages=PACKAGES,
+    package_data=PACKAGE_DATA,
     include_package_data=True,
-
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
-
     cmdclass=CMDCLASS,
-
     entry_points=ENTRY_POINTS,
 )
-
