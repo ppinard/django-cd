2d4329fae09ce1a4f46f992251b7e5998958303b
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-20 21:20:39 +0000

    Update views with testresults

--- django_cd/views.py
+++ django_cd/views.py
@@ -8,10 +8,11 @@ from django.shortcuts import render
 from django.utils import timezone
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 # Local modules.
 from .models import JobRun, RunState
-from .plotlyutil import render_plotly
+from .utils import render_plotly
 
 # Globals and constants variables.
 colors = {
@@ -38,10 +39,19 @@ def jobruns(request):
 
     df = pd.DataFrame(entries.values_list(*fields, named=True))
     df["ended_on"] = df["started_on"] + df["duration"]
+
+    # Create a fake ended_on to make sure the job is visible
+    df["fake_ended_on"] = df["started_on"].shift(1)
+    df["fake_ended_on"][0] = df["ended_on"][0]
     df["fake_ended_on"] = df.apply(
-        lambda x: x["started_on"] + max(datetime.timedelta(minutes=10), x["duration"]),
+        lambda x: min(
+            x["fake_ended_on"],
+            max(x["ended_on"], x["started_on"] + datetime.timedelta(minutes=10)),
+        ),
         axis="columns",
     )
+
+    print(df)
 
     xstart = df["started_on"].min()
     now = timezone.now()
@@ -52,12 +62,13 @@ def jobruns(request):
         x_end="fake_ended_on",
         y="name",
         hover_data={
+            "id": True,
             "name": False,
             "started_on": "|%Y-%m-%d %H:%M:%S",
             "duration": False,
             "ended_on": "|%Y-%m-%d %H:%M:%S",
-            "fake_ended_on": False,
             "state": False,
+            "fake_ended_on": False,
         },
         color="state",
         color_discrete_map=colors,
@@ -83,6 +94,10 @@ def jobruns(request):
         rangeslider={"range": [xstart, now]},
     )
     fig.update_yaxes(title={"text": "", "font": {"size": 18}})
-    fig.update_traces(selected={"marker": {"color": "red"}})
+    fig.update_traces(
+        hovertemplate="<b>Job #%{customdata[0]}</b><br>Started on: %{base|%Y-%m-%d %H:%M:%S}<br>Ended on: %{customdata[2]|%Y-%m-%d %H:%M:%S}<extra></extra>"
+    )
 
-    return render_plotly(request, fig.data, fig.layout)
+    return render_plotly(
+        request, fig.data, fig.layout, template="django_cd/jobruns_graph.html"
+    )
