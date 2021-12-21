b20e2a7807ea371c2b29b52be7ebc3eaded073e9
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-21 22:28:11 +0000

    Update views

--- django_cd/views.py
+++ django_cd/views.py
@@ -1,18 +1,12 @@
 """"""
 
 # Standard library modules.
-import datetime
 
 # Third party modules.
 from django.shortcuts import render
-from django.utils import timezone
-import plotly.express as px
-import pandas as pd
-import numpy as np
 
 # Local modules.
 from .models import JobRun, RunState
-from .utils import render_plotly
 
 # Globals and constants variables.
 colors = {
@@ -33,69 +27,27 @@ def jobrun(request, id):
     return render(request, "django_cd/jobrun.html", context={"jobrun": jobrun})
 
 
+def always_n(iterable, n):
+    it = iter(iterable)
+    for _ in range(n):
+        try:
+            yield next(it)
+        except StopIteration:
+            yield None
+
+
 def jobruns(request):
-    entries = JobRun.objects.order_by("-started_on")[:100]
-    fields = ["id", "name", "started_on", "duration", "state"]
+    names = sorted(JobRun.objects.values_list("name", flat=True).distinct().all())
 
-    df = pd.DataFrame(entries.values_list(*fields, named=True))
-    df["ended_on"] = df["started_on"] + df["duration"]
+    ordered_jobruns = {}
+    for name in names:
+        ordered_jobruns[name] = always_n(
+            JobRun.objects.filter(name=name)
+            .order_by("-started_on")
+            .values_list("id", "started_on", "state", named=True),
+            10,
+        )
 
-    # Create a fake ended_on to make sure the job is visible
-    df["fake_ended_on"] = df["started_on"].shift(1)
-    df["fake_ended_on"][0] = df["ended_on"][0]
-    df["fake_ended_on"] = df.apply(
-        lambda x: min(
-            x["fake_ended_on"],
-            max(x["ended_on"], x["started_on"] + datetime.timedelta(minutes=10)),
-        ),
-        axis="columns",
-    )
-
-    xstart = df["started_on"].min()
-    now = timezone.now()
-
-    fig = px.timeline(
-        df,
-        x_start="started_on",
-        x_end="fake_ended_on",
-        y="name",
-        hover_data={
-            "id": True,
-            "name": False,
-            "started_on": "|%Y-%m-%d %H:%M:%S",
-            "duration": False,
-            "ended_on": "|%Y-%m-%d %H:%M:%S",
-            "state": False,
-            "fake_ended_on": False,
-        },
-        color="state",
-        color_discrete_map=colors,
-        custom_data=["id"],
-    )
-    fig.update_xaxes(
-        autorange=False,
-        range=[now - datetime.timedelta(days=1), now],
-        rangeselector={
-            "buttons": [
-                {
-                    "count": 10,
-                    "label": "10min",
-                    "step": "minute",
-                    "stepmode": "backward",
-                },
-                {"count": 1, "label": "1h", "step": "hour", "stepmode": "backward"},
-                {"count": 1, "label": "1d", "step": "day", "stepmode": "backward"},
-                {"count": 1, "label": "1m", "step": "month", "stepmode": "backward"},
-                {"step": "all"},
-            ]
-        },
-        rangeslider={"range": [xstart, now]},
-    )
-    fig.update_yaxes(title={"text": "", "font": {"size": 18}})
-    fig.update_traces(
-        hovertemplate="<b>Job #%{customdata[0]}</b><br>Started on: %{base|%Y-%m-%d %H:%M:%S}<br>Ended on: %{customdata[2]|%Y-%m-%d %H:%M:%S}<extra></extra>"
-    )
-
-    return render_plotly(
-        request, fig.data, fig.layout, template="django_cd/jobruns_graph.html"
+    return render(
+        request, "django_cd/jobruns.html", context={"ordered_jobruns": ordered_jobruns}
     )
