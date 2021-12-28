7719ee2af3e1f5f035adcaff916bfa87b6e7ef8e
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-28 12:59:24 +0000

    improve app config

--- django_cd/apps.py
+++ django_cd/apps.py
@@ -18,7 +18,7 @@ class DjangoCdConfig(AppConfig):
     name = "django_cd"
 
     def ready(self):
-        if "manage.py" in sys.argv and "runserver" not in sys.argv:
+        if any("manage.py" in sys.argv) and "runserver" not in sys.argv:
             return
 
         from .jobs import Job
