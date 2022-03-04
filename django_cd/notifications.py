1bc9304466ace12867df3b18a8ef7f204b9744b4
Philippe Pinard <philippe.pinard@gmail.com>
2022-03-04 13:08:20 +0000

    Add argument to only send notifications on failure or success

--- django_cd/notifications.py
+++ django_cd/notifications.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mail
 from django.template import Engine, Context
 
 # Local modules.
+from .models import RunState
 
 # Globals and constants variables.
 
@@ -19,22 +20,27 @@ class Notification(metaclass=abc.ABCMeta):
 
 
 class EmailNotification(Notification):
-    def __init__(self, recipients):
+    def __init__(self, recipients, on_success=False, on_failure=True):
         self.recipients = tuple(recipients)
+        self.on_success = on_success
+        self.on_failure = on_failure
 
     def __str__(self):
         return "email"
 
     def notify(self, jobrun):
-        engine = Engine.get_default()
-        template = engine.get_template("django_cd/jobrun_report.html")
-        context = Context({"jobrun": jobrun})
+        if (jobrun.state in [RunState.ERROR, RunState.FAILED] and self.on_failure) or (
+            jobrun.state == RunState.SUCCESS and self.on_success
+        ):
+            engine = Engine.get_default()
+            template = engine.get_template("django_cd/jobrun_report.html")
+            context = Context({"jobrun": jobrun})
 
-        html_message = template.render(context)
-        send_mail(
-            subject=f"Job report - {jobrun.name} - {jobrun.state}",
-            message="",
-            from_email=None,
-            recipient_list=self.recipients,
-            html_message=html_message,
-        )
+            html_message = template.render(context)
+            send_mail(
+                subject=f"Job report - {jobrun.name} - {jobrun.state}",
+                message="",
+                from_email=None,
+                recipient_list=self.recipients,
+                html_message=html_message,
+            )
